db1c8dd9377f00f39470da20ac9adf32baad396d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-16 22:36:05 +0530

    added logout function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import api from "./api";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export async function logout() {
+  try {
+    const response = await api.post('/api/user/logout', {
+
+    }, { withCredentials: true })
+
+    if (response.status > 200) {
+      throw new Error('Logout failed');
+    }
+
+    window.location.href = '/login';
+  } catch (error) {
+    console.error('Logout error:', error);
+    throw error;
+  }
 }
