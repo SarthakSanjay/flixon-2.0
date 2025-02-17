f565dd934936cf319c0882ca981018e818baa9f2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-17 23:55:21 +0530

    add: documentry page

---
+++ src/app/(home)/documentry/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Content from "@/components/app-content";
+import { usePathname } from "next/navigation";
+
+export default function page() {
+  const pathname = usePathname()
+  return (
+    <Content pathname={pathname} />
+  )
+}
