6602c1667c7113baf7a54f267c110c79571b067e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-18 23:01:13 +0530

    refactor: removed navigation options

--- src/components/app-navbar.tsx
+++ src/components/app-navbar.tsx
@@ -1,45 +1,14 @@
 "use client";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu"
-import { usePathname, useRouter } from "next/navigation"
-import { cn } from "@/lib/utils"
 import ProfileDropdown from "./app-profile-dropdown";
+import { AppSidebar } from "./app-sidebar";
+import Logo from "./app-logo";
 
 export function Navbar() {
-  const router = useRouter()
-  const pathname = usePathname()
-
-  const navItems = [
-    { path: '/movies', label: 'Movies' },
-    { path: '/shows', label: 'TV Series' },
-    { path: '/documentry', label: 'Documentry' }
-  ]
-
   return (
-    <div className="h-max w-full grid place-content-center relative">
-      <NavigationMenu className="h-fit w-full p-2 border-0 border-white flex justify-center items-center">
-        <NavigationMenuList>
-          {navItems.map((item) => (
-            <NavigationMenuItem
-              key={item.path}
-              className={cn(
-                "h-10 px-5 rounded-lg grid place-content-center transition-colors",
-                "hover:bg-teal-500/50 active:bg-teal-500",
-                pathname === item.path && "bg-teal-500"
-              )}
-              onClick={() => router.push(item.path)}
-            >
-              {item.label}
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div className="h-14 bg-gradient-to-b from-black to-black/0 w-full px-10 flex items-center justify-between absolute z-20 ">
+      <AppSidebar isAdminDashboard={false} />
+      <Logo />
       <ProfileDropdown />
     </div>
-
-  )
+  );
 }
