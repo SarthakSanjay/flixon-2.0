682fb2eb6a82bfeeface9e0ca77f71e093b49ba8
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-18 22:57:38 +0530

    add: app logo

---
+++ src/components/app-logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div className="h-14 w-24  grid place-content-center">
+      <img src="/logo/applogo.png" className="w-full" />
+    </div>
+  );
+}
