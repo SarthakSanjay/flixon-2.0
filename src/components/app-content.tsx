a1c5f59e6b069e5ef07bad5b6f004480ed8d6a97
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-18 22:59:29 +0530

    refactor: include menubar

--- src/components/app-content.tsx
+++ src/components/app-content.tsx
@@ -1,41 +1,23 @@
-import { useEffect, useState } from "react";
 import HeroContent from "./app-hero-content";
-import { ScrollArea } from "./ui/scroll-area";
-import api from "@/lib/api";
-import ContentCard from "./app-content-card";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 import ContentCarousel from "./app-content-carousel";
 import { Navbar } from "./app-navbar";
+import { Menubar } from "./app-menubar";
 
 export default function Content({ pathname }: { pathname: string }) {
-  const [contents, setContents] = useState([])
-  useEffect(() => {
-    const fetchContent = async () => {
-      const res = await api.get(`/api${pathname}`, { withCredentials: true })
-      console.log(res.data)
-      setContents(res.data?.movies)
-    }
-    // fetchContent()
-  }, [])
-
-  const genres = [
-    "Trending",
-    "Action",
-    "Romance",
-    "Horror"
-  ]
+  const genres = ["Action", "Romance", "Horror"];
 
   return (
-    <div className="h-[calc(100vh-64px)] w-full">
-      <ScrollArea className="h-full rounded-md mt-14">
+    <div className="h-[100vh] border-0 border-green-500 w-full bg-black overflow-y-scroll">
+      <div className="h-full w-full border-0 border-blue-500 ">
         <Navbar />
         <HeroContent />
-        {
-          genres.map((genre, index) => {
-            return <ContentCarousel key={index} genre={genre} />
-          })
-        }
 
-      </ScrollArea>
+        {genres.map((genre, index) => {
+          return <ContentCarousel key={index} genre={genre} />;
+        })}
+      </div>
+      <Menubar />
     </div>
   );
 }
