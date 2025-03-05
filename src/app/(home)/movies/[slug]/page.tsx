5039d9096532c84a12d114374919e0314dadc412
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-05 23:55:31 +0530

    refactor

--- src/app/(home)/movies/[slug]/page.tsx
+++ src/app/(home)/movies/[slug]/page.tsx
@@ -1,5 +1,16 @@
+import ContentByGenre from "@/components/app-content-by-genre";
 import ContentDetails from "@/components/app-content-details";
 
-export default function page() {
-  return <ContentDetails />;
+type MovieDetailsPageProp = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function page({ params }: MovieDetailsPageProp) {
+  const genre = params.slug.split("-");
+  if (genre[0] === "genre") {
+    return <ContentByGenre genre={genre[1]} />;
+  }
+  return <ContentDetails movieId={params.slug} />;
 }
