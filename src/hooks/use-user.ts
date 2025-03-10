3a44424cc0aec84b52e72b500ac79f96daf6bf4e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-11 03:30:10 +0530

    feat: add getProfileById function to fetch profile

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -1,4 +1,5 @@
 import api from "@/lib/api";
+import { ProfileResponse, SingleProfileRes } from "@/types/response";
 import { useState } from "react";
 
 export default function useUser() {
@@ -8,8 +9,8 @@ export default function useUser() {
   const getUserProfiles = async (userId: string) => {
     setLoading(true);
     try {
-      const res = await api.get(`/api/profile/${userId}`);
-      return res.data;
+      const res = await api.get(`/api/user/profiles/${userId}`);
+      return res.data as ProfileResponse;
     } catch (error: any) {
       if (error.response) {
         setError("Error fetching user profiles");
@@ -19,10 +20,17 @@ export default function useUser() {
     }
   };
 
-  const createUserProfile = async (userId: string) => {
+  const createUserProfile = async (
+    userId: string,
+    username: string,
+    avatar: string,
+  ) => {
     setLoading(true);
     try {
-      const res = await api.post(`/api/profile/${userId}`);
+      const res = await api.post(`/api/profile/${userId}`, {
+        name: username,
+        avatar: avatar,
+      });
       return res.data;
     } catch (error: any) {
       if (error.response) {
@@ -32,5 +40,25 @@ export default function useUser() {
       setLoading(false);
     }
   };
-  return { getUserProfiles, createUserProfile, loading, error };
+
+  const getProfileById = async (profileId: string) => {
+    setLoading(true);
+    try {
+      const res = await api.get(`/api/profile/${profileId}`);
+      return res.data as SingleProfileRes;
+    } catch (error: any) {
+      if (error.response) {
+        setError("Error fetching user profile ");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return {
+    getUserProfiles,
+    createUserProfile,
+    getProfileById,
+    loading,
+    error,
+  };
 }
