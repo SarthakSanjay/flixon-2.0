bd495b6a02303a4ddfc742d7d530fb4c02726b9a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-12 23:35:59 +0530

    refactor

--- src/components/buttons/add-to-watchlist-btn.tsx
+++ src/components/buttons/add-to-watchlist-btn.tsx
@@ -4,22 +4,75 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { Button } from "../ui/button"
+} from "@/components/ui/tooltip";
+import { Button } from "../ui/button";
+import { useWatchlist } from "@/hooks/use-watchlist";
+import { useEffect, useState } from "react";
+import Loading from "../loading";
 
+export default function AddToWatchList({ contentId }: { contentId?: string }) {
+  const { addToWatchlist, getWatchlist, removeFromWatchlist, loading, error } =
+    useWatchlist();
+  const [isPresent, setIsPresent] = useState(false);
 
-export default function AddToWatchList() {
+  const handleClick = async () => {
+    const profileId = localStorage.getItem("profileId");
+
+    if (profileId && contentId) {
+      if (isPresent) {
+        await removeFromWatchlist(profileId, contentId);
+        setIsPresent(false);
+      } else {
+        await addToWatchlist(contentId, profileId);
+        setIsPresent(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log("inside watchlist button");
+    const fetchWatchlist = async () => {
+      const profileId = localStorage.getItem("profileId");
+      if (profileId) {
+        const res = await getWatchlist(profileId);
+        if (res) {
+          setIsPresent(
+            res?.watchlist.some((content) => content._id === contentId),
+          );
+        }
+      }
+    };
+    fetchWatchlist();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
-          <Button className="h-12 w-12 text-3xl rounded-lg bg-black/50 text-white">
-            +
-          </Button>
+          {isPresent ? (
+            <Button
+              className="h-12 rounded-lg bg-black/50 text-white"
+              onClick={handleClick}
+            >
+              Remove from watchlist
+            </Button>
+          ) : (
+            <Button
+              className="h-12 w-12 text-3xl rounded-lg bg-black/50 text-white"
+              onClick={handleClick}
+            >
+              +
+            </Button>
+          )}
         </TooltipTrigger>
-        <TooltipContent>Add to watchlist</TooltipContent>
+        <TooltipContent>
+          {isPresent ? "Remove form watchlist" : "Add to watchlist"}
+        </TooltipContent>
       </Tooltip>
     </TooltipProvider>
-
-  )
+  );
 }
