e16947a12c9f1662adb2cbb5f77d26ff0976682b
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-13 23:59:08 +0530

    refactor

--- src/app/(home)/movies/page.tsx
+++ src/app/(home)/movies/page.tsx
@@ -1,12 +1,8 @@
 "use client";
-import { userState } from "@/atoms/atom";
 import Content from "@/components/app-content";
 import { usePathname } from "next/navigation";
-import { useRecoilState } from "recoil";
 
 export default function page() {
   const pathname = usePathname();
-  // const [user, setUser] = useRecoilState(userState);
-  // console.log("user from state ", user);
   return <Content pathname={pathname} />;
 }
