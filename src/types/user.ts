1d79a34f1daddb315fc1407d63bd0d95bbfdc2a0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-14 23:55:12 +0530

    feat:add user types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,9 +1,20 @@
 import { History, Watchlist } from "./movie";
 
 export interface ProfileProps {
-  id: string;
+  _id: string;
   name: string;
   avatar: string;
   watchlist: Watchlist[];
   history: History[];
+}
+export interface UserProp {
+  message: string;
+  user: User;
+}
+export interface User {
+  _id: string;
+  email: string;
+  password: string;
+  phone: string;
+  createdAt: string;
 }
