4ef2c3682cc94c5ea993105c79636014bbce0492
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-18 23:19:56 +0530

    feat: add format date function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,15 @@
 export function capatilizedFirstLetter(str: string) {
   return `${str.charAt(0).toUpperCase() + str.slice(1)}`;
 }
+
+export function formatDate(str: string) {
+  const date = new Date(str);
+
+  const day = date.getDate();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+
+  const formattedDate = `${day} ${month} ${year}`;
+
+  return formattedDate;
+}
