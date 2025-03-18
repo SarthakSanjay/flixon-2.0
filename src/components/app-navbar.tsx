0fc4d8a84558131ea5475bd2d7ad11bb8a66f5a2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-18 23:18:48 +0530

    refactor

--- src/components/app-navbar.tsx
+++ src/components/app-navbar.tsx
@@ -5,7 +5,7 @@ import ProfileDropdown from "./profile/app-profile-dropdown";
 
 export function Navbar() {
   return (
-    <div className="h-14 bg-gradient-to-b from-black to-black/0 w-full px-10 flex items-center justify-between absolute z-20 ">
+    <div className="h-14 bg-gradient-to-b from-black to-black/0 w-full px-10 flex items-center justify-between absolute z-50 ">
       <AppSidebar isAdminDashboard={false} />
       <Logo />
       <ProfileDropdown />
