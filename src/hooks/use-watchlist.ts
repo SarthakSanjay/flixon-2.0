03991480206022f55052e4964c7e9724ef84f4ac
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-05 23:03:00 +0530

    feat: add new hook to fetch shows,renamed getWatchlist to getWatchlistMovies

--- src/hooks/use-watchlist.ts
+++ src/hooks/use-watchlist.ts
@@ -1,5 +1,8 @@
 import api from "@/lib/api";
-import { WatchlistResponse } from "@/types/response";
+import {
+  WatchlistMovieResponse,
+  WatchlistShowResponse,
+} from "@/types/response";
 import { useState } from "react";
 
 export function useWatchlist() {
@@ -23,11 +26,11 @@ export function useWatchlist() {
     }
   };
 
-  const getWatchlist = async (profileId: string) => {
+  const getWatchlistMovies = async (profileId: string) => {
     setLoading(true);
     try {
-      const res = await api.get(`/api/watchlist/${profileId}`);
-      return res.data as WatchlistResponse;
+      const res = await api.get(`/api/watchlist/movie/${profileId}`);
+      return res.data as WatchlistMovieResponse;
     } catch (error: any) {
       if (error.response) {
         setError("Error fetching movie");
@@ -51,10 +54,25 @@ export function useWatchlist() {
     }
   };
 
+  const getWatchlistShows = async (profileId: string) => {
+    setLoading(true);
+    try {
+      const res = await api.get(`/api/watchlist/show/${profileId}`);
+      return res.data as WatchlistShowResponse;
+    } catch (error: any) {
+      if (error.response) {
+        setError("Error fetching shows");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     addToWatchlist,
-    getWatchlist,
+    getWatchlistMovies,
     removeFromWatchlist,
+    getWatchlistShows,
     loading,
     error,
   };
