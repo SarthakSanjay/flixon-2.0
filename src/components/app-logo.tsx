156e35ed3e7d835f04bbf5a63a4dc568482eeb33
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-06 22:42:12 +0530

    update: when logo is clicked navigate to home route

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -1,6 +1,13 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Logo() {
+  const router = useRouter();
   return (
-    <div className="h-14 w-24  grid place-content-center">
+    <div
+      className="h-14 w-24  grid place-content-center cursor-pointer"
+      onClick={() => router.push("/")}
+    >
       <img src="/logo/applogo.png" className="w-full" />
     </div>
   );
