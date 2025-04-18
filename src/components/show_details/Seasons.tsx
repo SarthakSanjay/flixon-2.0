23a384fb88c9082109bbd53ed1ec29e13bbc34ea
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-18 21:14:01 +0530

    feat: add seasons component

---
+++ src/components/show_details/Seasons.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import useContent from "@/hooks/use-content";
+import { Season } from "@/types/show";
+import Loading from "../loading";
+import { useAtom } from "jotai";
+import { currentSeasonAtom } from "@/atoms/atom";
+
+export default function Seasons({ showId }: { showId: string }) {
+  const { getShowsSeason, loading } = useContent();
+  const [seasons, setSeasons] = useState<Season[]>([]);
+  const [currentSeason, setCurrentSeason] = useAtom(currentSeasonAtom);
+
+  useEffect(() => {
+    async function fetchSeason() {
+      const res = await getShowsSeason(showId);
+      if (res) {
+        setSeasons(res.data);
+        setCurrentSeason(res.data[0]);
+      }
+    }
+    fetchSeason();
+  }, []);
+
+  if (loading) <Loading />;
+
+  return (
+    <div className="h-12 w-full flex gap-5">
+      {seasons.map((season) => {
+        return (
+          <Button
+            key={season._id}
+            className={cn(
+              "text-white text-lg bg-white/20",
+              season._id === currentSeason._id ? "bg-orange-500/20" : "",
+            )}
+            onClick={() => setCurrentSeason(season)}
+          >
+            Season {season.number}
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
