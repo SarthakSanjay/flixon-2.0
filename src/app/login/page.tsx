bc8dc16bbaee0faf8808604406aae88720e47015
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-19 23:48:28 +0530

    update: add background image

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,15 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
 import { LoginForm } from "@/components/login-form";
 import Logo from "@/components/app-logo";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh bg-black flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex items-center w-full max-w-sm flex-col gap-6">
+    <div className="flex min-h-svh bg-black flex-col items-center justify-center gap-6 p-6 md:p-10 relative">
+      <img
+        src="./background.jpg"
+        className="h-full w-full absolute top-0 left-0 z-0"
+      />
+      <div className="h-full w-full absolute top-0 left-0 z-10 bg-black/70"></div>
+      <div className="flex items-center w-full max-w-sm flex-col gap-6 z-20">
         <Logo />
         <LoginForm />
       </div>
