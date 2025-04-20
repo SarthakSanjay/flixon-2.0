211efb520c06af44ebf10e0d6aa8110c25328b89
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-20 21:56:11 +0530

    refactor: add addMovieToFavorite, addShowToFavorite, getAllFavoriteShows, getAllFavoriteMovies func

--- src/hooks/use-favorite.ts
+++ src/hooks/use-favorite.ts
@@ -6,17 +6,34 @@ export default function useFavorite() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const addContentToFavorite = async (profileId: string, contentId: string) => {
+  const addMovieToFavorite = async (profileId: string, contentId: string) => {
     setLoading(true);
     try {
-      const res = await api.post(`/api/favorite`, {
+      const res = await api.post(`/api/favorite/movie`, {
         profileId: profileId,
         contentId: contentId,
       });
       return res.data;
     } catch (error: any) {
       if (error.response) {
-        setError("Error fetching movie");
+        setError("Error adding movie to favorite");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addShowToFavorite = async (profileId: string, contentId: string) => {
+    setLoading(true);
+    try {
+      const res = await api.post(`/api/favorite/show`, {
+        profileId: profileId,
+        contentId: contentId,
+      });
+      return res.data;
+    } catch (error: any) {
+      if (error.response) {
+        setError("Error adding show to favorite");
       }
     } finally {
       setLoading(false);
@@ -40,10 +57,10 @@ export default function useFavorite() {
     }
   };
 
-  const getAllUserFavorite = async (profileId: string) => {
+  const getAllFavoriteMovies = async (profileId: string) => {
     setLoading(true);
     try {
-      const res = await api.get(`/api/favorites/${profileId}`);
+      const res = await api.get(`/api/favorites/movie/${profileId}`);
       return res.data as FavoriteResponse;
     } catch (error: any) {
       if (error.response) {
@@ -54,10 +71,26 @@ export default function useFavorite() {
     }
   };
 
+  const getAllFavoriteShows = async (profileId: string) => {
+    setLoading(true);
+    try {
+      const res = await api.get(`/api/favorites/show/${profileId}`);
+      return res.data as FavoriteResponse;
+    } catch (error: any) {
+      if (error.response) {
+        setError("Error fetching shows");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
-    addContentToFavorite,
+    addMovieToFavorite,
+    addShowToFavorite,
     removeContentFromFavorite,
-    getAllUserFavorite,
+    getAllFavoriteMovies,
+    getAllFavoriteShows,
     loading,
     error,
   };
